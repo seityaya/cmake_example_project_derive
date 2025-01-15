fa031890be6fe1320529f9a0059e944eede2cbfb
Terlekchi Seityaya <terlekchi.seityaya@gmail.com>
2025-01-15 17:23:28 +0300

    Refactoring

--- core/my_calc.c
+++ core/my_calc.c
@@ -1,3 +1,10 @@
+// Author                 : Seityagiya Terlekchi
+// Contacts               : terlekchiseityaya@gmail.com
+// Creation Date          : 2024.10
+// License Link           : https://spdx.org/licenses/LGPL-2.1-or-later.html
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// Copyright © 2024-2025 Seityagiya Terlekchi. All rights reserved.
+
 #include "my_math.h"
 
 int my_calculator(char op, int a, int b) {
